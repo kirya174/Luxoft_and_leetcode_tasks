663e0e261485a358991c2fc4122797246e97b96d
Kirill <kirya174@gmail.com>
2022-04-06 12:36:38 +0300

    "Updates"

--- binary_search.py
+++ binary_search.py
@@ -23,13 +23,14 @@ def isBadVersion(version: int) -> bool:
     if version == 1:
         return True
 
+
 import math
 
 min_n = 0
 # mid_n = n//2
 max_n = 1
 while True:
-    mid_n = math.ceil((max_n + min_n)/2)
+    mid_n = math.ceil((max_n + min_n) / 2)
     if isBadVersion(mid_n):
         if isBadVersion(mid_n - 1):
             max_n = mid_n
@@ -37,4 +38,3 @@ while True:
             print(mid_n)
     else:
         min_n = mid_n
-
