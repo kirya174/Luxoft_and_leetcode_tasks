2021eb69f683fa5419cb3961e5c100d84be6ea15
Kirill <kirya174@gmail.com>
2022-05-23 11:23:55 +0300

    remove Nth node function finished

--- Leetcode/Nodes.py
+++ Leetcode/Nodes.py
@@ -4,47 +4,48 @@ class ListNode:
         self.next = next
 
 
-node = ListNode(1, None)
-node = ListNode(2, node)
-node = ListNode(3, node)
-node = ListNode(4, node)
-node = ListNode(5, node)
+def create_node(vals: list) -> ListNode:
+    node = None
+    for i in range(len(vals)):
+        node = ListNode(vals[i], node if node is not None else None)
+    return node
 
 
-# class Solution1:
-#     """ returns middle node of a linked list """
-#     def middleNode(self, head: [ListNode]) -> [ListNode]:
-#         counter = 0
-#         mid_node_nr = 0
-#         mid_node = head
-#         last_node = head
-#         while last_node.next is not None:
-#             last_node = last_node.next
-#             if counter // 2 >= mid_node_nr:
-#                 mid_node_nr += 1
-#                 mid_node = mid_node.next
-#             counter += 1
-#         return mid_node.val
-#
-#
-# print(Solution1().middleNode(node))
+node1 = create_node([1, 2, 3, 4, 5])
+node2 = create_node([3, 7, 9, 3, 5, 8, 0])
 
 
 class Solution:
+    """ returns middle node of a linked list """
+
+    def middleNode(self, head: [ListNode]) -> [ListNode]:
+        counter = 0
+        mid_node_nr = 0
+        mid_node = head
+        last_node = head
+        while last_node.next is not None:
+            last_node = last_node.next
+            if counter // 2 >= mid_node_nr:
+                mid_node_nr += 1
+                mid_node = mid_node.next
+            counter += 1
+        return mid_node.val
+
     def removeNthFromEnd(self, head: [ListNode], n: int) -> [ListNode]:
-        count = 0
+        list_node_length = 0
+        vals = []
         node = head
         while node is not None:
+            vals.append(node.val)
             node = node.next
-            count += 1
-
-        node = head
-        for _ in range(count - n):
-            node = node.next
-        node = node.next.next
-        while node.next is not None:
-            node = node.next
+            list_node_length += 1
+        vals.pop(list_node_length - n)
+        vals.reverse()
+        node = create_node(vals)
         return node
 
 
-print(Solution().removeNthFromEnd(node, 2))
+# print(Solution().middleNode(node))
+
+node1 = Solution().removeNthFromEnd(node1, 2)
+node2 = Solution().removeNthFromEnd(node2, 1)
